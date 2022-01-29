e2f12b5a1df6465a08e5098b23f9f5ed0c20f594
RahatGithub <rahatahmedchowdhuryrac@gmail.com>
2022-01-30 00:18:33 +0600

    displaySingleItem() added

--- script.js
+++ script.js
@@ -18,13 +18,11 @@ const loadDefault = () => {
         for (let i=0; i<Object.keys(data.categories).length; i++){
             const category = data.categories[i];
             const categoryName = category.strCategory;
-            const description = category.strCategoryDescription;
             const thumbnail = category.strCategoryThumb;
             const div = document.createElement('div');
             div.innerHTML = `
                 <img class="thumbnail" src="${thumbnail}" alt="thumbnail">
                 <h3>${categoryName}</h3>
-                <p>${description.slice(0, 100)}...</p>
             `;
             div.className = "single-item";
             const section = document.getElementById('info-section');
@@ -35,14 +33,37 @@ const loadDefault = () => {
 
 loadDefault();
 
+
 const loadCatAndArea = input => {
+
     //fetching all the categories
     fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
     .then(res => res.json())
     .then(data => {
         // console.log(data.meals);
         const categories = data.meals;
-        matchCategory(categories, input);
+        const category = matchCategory(categories, input);
+        if(category){
+            fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c='+category)
+            .then(res => res.json())
+            .then(data => {
+                const section = document.getElementById('extra-info-section');
+                section.innerHTML = '';
+                for (let i=0; i<Object.keys(data.meals).length; i++){
+                    const items = data.meals[i];
+                    const name = items.strMeal;
+                    const thumbnail = items.strMealThumb;
+                    const div = document.createElement('div');
+                    div.innerHTML = `
+                        <img class="thumbnail" src="${thumbnail}" alt="thumbnail">
+                        <h3>${name}</h3>
+                        <button onclick="displaySingleItem('${name}')">More</button>
+                    `;
+                    div.className = "single-item";
+                    section.appendChild(div);
+                }
+            })
+        }
     })
 
     //fetching all the areas
@@ -51,26 +72,76 @@ const loadCatAndArea = input => {
     .then(data => {
         // console.log(data.meals);
         const areas = data.meals;
-        matchArea(areas, input);
+        const area = matchArea(areas, input);
+        if(area){
+            fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a='+area)
+            .then(res => res.json())
+            .then(data => {
+                const section = document.getElementById('extra-info-section');
+                section.innerHTML = '';
+                for (let i=0; i<Object.keys(data.meals).length; i++){
+                    const items = data.meals[i];
+                    const name = items.strMeal;
+                    const thumbnail = items.strMealThumb;
+                    const div = document.createElement('div');
+                    div.innerHTML = `
+                        <img class="thumbnail" src="${thumbnail}" alt="thumbnail">
+                        <h3>${name}</h3>
+                        <button onclick="displaySingleItem('${name}')">More</button>
+                    `;
+                    div.className = "single-item";
+                    section.appendChild(div);
+                }
+            })
+        }
     })
 }
+
 
 const matchCategory = (data, input) => {
     for(let i=0; i<Object.keys(data).length; i++){
         // console.log(data[i].strCategory, input);
         if (input == data[i].strCategory){
-            console.log(input + " found");
-            break;
+            return data[i].strCategory;
         }
     }
 }
+
 
 const matchArea = (data, input) => {
     for(let i=0; i<Object.keys(data).length; i++){
         // console.log(data[i].strCategory, input);
         if (input == data[i].strArea){
-            console.log(input + " found");
-            break;
+            return data[i].strArea;
         }
     }
+}
+
+const displaySingleItem = name => {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s='+name)
+    .then(res => res.json())
+    .then(data => {
+        const section = document.getElementById('single-item-section');
+        section.innerHTML = '';
+        const meal = data.meals[0];
+        const name = meal.strMeal;
+        const thumbnail = meal.strMealThumb;
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <img src="${thumbnail}" class="thumbnail">
+            <h1>${name}</h1>
+            <ul id="ingredient-list"></ul>
+        `;
+        div.className = 'single-item';
+        section.appendChild(div);
+        const ingredientList = document.getElementById('ingredient-list');
+        for(let i=1; i<=20; i++){
+            const ingredient = 'strIngredient'+i
+            if(meal[ingredient]){
+                const item = document.createElement('li');
+                item.innerText = meal[ingredient];
+                ingredientList.appendChild(item);
+            }
+        }
+    })
 }
